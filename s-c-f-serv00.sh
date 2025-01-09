e9dda97ea9d837428fbde5c4a2fe05ec0267f3eb
20241204 <18745620828@189.cn>
2025-01-10 07:30:04 +0800

    Update s-c-f-serv00.sh

--- s-c-f-serv00.sh
+++ s-c-f-serv00.sh
@@ -2108,6 +2108,7 @@ rules:
 pkill sing-box-freebsd &
 # kill -9 \$(ps | grep -v grep | grep cloudflared-freebsd | awk '{print \$1}')
 pkill cloudflared-freebsd &
+sleep 3
 nohup ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/sing-box-freebsd run -c ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/config.json > ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/sing-box.log 2>&1 & disown
 sleep 5
 nohup ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared-freebsd tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH} > ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared.log 2>&1 & disown
